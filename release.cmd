d52982e72304b83e14416681b9d7a89d490b5bab
JustOff <Off.Just.Off@gmail.com>
2021-02-22 18:51:03 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.3.0
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/password-backup-tool-.+?\.xpi/download\/%VER%\/password-backup-tool-%VER%\.xpi/" update.xml
+
+set XPI=password-backup-tool-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
